6a45c68171a026c7cbabe52628c2ad3f1fa63404
bas <bas@83a8008e-574f-11de-8832-3f4c773c73ae>
2007-12-13 08:59:29 +0000

    Next-gen editing page :)

--- adm/admin_node.php
+++ adm/admin_node.php
@@ -21,11 +21,11 @@ $node->read();
 
 echo '<h1>' . sprintf(__('Actions for %s'), $node->title) . '</h1>';
 ?>
-<p class="icon_p"><a href="admin_node_new.php?node=<?php echo $node->node_id ?>"><img src="images/add.png" /><br /><?php echo __('Add new child node') ?></a><br /><?php echo __('Add a new child node to this node.') ?></p>
+<p class="icon_p"><a href="admin_node_new.php?node=<?php echo $node->node_id ?>&amp;do=new"><img src="images/add.png" /><br /><?php echo __('Add new child node') ?></a><br /><?php echo __('Add a new child node to this node.') ?></p>
 <p class="icon_p"><a href="admin_node_modules.php?node=<?php echo $node->node_id ?>"><img src="images/modules.png" /><br /><?php echo __('Edit modules') ?></a><br /><?php echo __('Add or edit the modules (content) in this node.') ?></p>
 <p class="icon_p"><a href="admin_node_options.php?node=<?php echo $node->node_id ?>"><img src="images/edit.png" /><br /><?php echo __('Edit options') ?></a><br /><?php echo __('Configure options in this node.') ?></p>
 <p class="icon_p"><a href="admin_node_revisions.php?node=<?php echo $node->node_id ?>"><img src="images/revisions.png" /><br /><?php echo __('Older versions') ?></a><br /><?php echo __('View older versions of this node.') ?></p>
-<p class="icon_p"><a href="admin_node_edit.php?node=<?php echo $node->node_id ?>"><img src="images/edit.png" /><br /><?php echo __('Edit node details') ?></a><br /><?php echo __('Edit the node details like title and description.') ?></p>
+<p class="icon_p"><a href="admin_node_new.php?node=<?php echo $node->node_id ?>&amp;do=edit"><img src="images/edit.png" /><br /><?php echo __('Edit node details') ?></a><br /><?php echo __('Edit the node details like title and description.') ?></p>
 <p class="icon_p"><a href="admin_node_delete.php?node=<?php echo $node->node_id ?>"><img src="images/edit_remove.png" /><br /><?php echo __('Delete node') ?></a><br /><?php echo __('Delete this node.') ?></p>
 <?php
 
