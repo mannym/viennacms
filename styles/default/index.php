cb8a8c5437eabb575e935b750ae13a4fe88ace93
bas <bas@83a8008e-574f-11de-8832-3f4c773c73ae>
2008-01-25 14:00:13 +0000

    my style version, index.php coming soon

--- styles/default/index.php
+++ styles/default/index.php
@@ -1,42 +1,39 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<title><?php echo $title; ?></title>
-		<?php echo $head; ?>
+		<title><?php echo $sitename ?> &bull; <?php echo $title ?></title>
+		<?php echo $head ?>
 		<link rel="stylesheet" href="<?php echo $stylesheet ?>" />
 	</head>
-
 	<body>
-		<div id="wrap">
-			<div id="header">
-				<a href="" class="logo"></a>
-				<span><a href="adm/"><?php echo __('ACP'); ?></a></span>
-			</div>
-			
-			<div id="menucontainer">
-				<div>
-					<ul id="menu">
-						<?php echo $nav_level1 ?>
-					</ul>
-				</div>
-			</div>
-			
-			<div id="content">
-				<span class="breadcrumbs"><?php echo $crumbs; ?></span>
-				<?php echo $middle; ?>
-			</div>
-			
-			<div id="sidebar">
-				<?php if(!empty($nav_level2)): ?><h1>Navigation</h1>
+	<div id="wrap">
+	<div id="header">
+		<a href="<?php echo $homeurl ?>" class="logo"></a>
+		<span><a href="adm/"><?php echo __('ACP') ?></a></span>
+	</div>
+	<div id="menucontainer">
+	<div>
+	<ul id="menu">
+<?php echo $nav_level1 ?>
+	</ul>
+	</div>
+	</div>
+	<div id="content">
+		<span class="breadcrumbs"><?php echo $crumbs ?></span>
+		<h1 id="pagetitle"><?php echo $title ?></h1>
+		<br style="clear: both;" />
+		<?php echo $middle ?>
+	</div>
+	<div id="sidebar">
+			<?php if ($nav_level2) : ?>
+				<h1><?php echo __('Navigation') ?></h1>
 				<ul>
 					<?php echo $nav_level2 ?>
-				</ul><?php endif; ?>
-				<?php if(!empty($right)) echo $right; ?>
-			</div>
-			
-			<div id="footer">
-				Copyright by <a href="http://www.viennainfo.nl/">viennaCMS</a>.
-			</div>
+				</ul>
+				<br />
+			<?php endif; ?>
+			<?php if ($right) : ?><?php echo $right ?><?php endif; ?>
 		</div>
+		<?php echo $footer ?>
 	</body>
 </html>
