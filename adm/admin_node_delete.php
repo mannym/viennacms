02caf498ccdbec6bde5cba1e4e257f824e4e5f92
salomon <salomon@83a8008e-574f-11de-8832-3f4c773c73ae>
2008-01-26 19:47:13 +0000

    Deleting works good now

--- adm/admin_node_delete.php
+++ adm/admin_node_delete.php
@@ -36,29 +36,22 @@ switch($mode) {
 		$row = $db->sql_fetchrow($result);
 		$site_count = $row['site_count'];
 		if($site_count < 1 || $site_count == 1) {
-			define('SITE_DELETE_LEGAL', true);
-		}
-		else {
 			define('SITE_DELETE_LEGAL', false);
 		}
-		
-		$sql1 = "DELETE FROM " . NODES_TABLE . "
-				 WHERE node_id = " . $node_id;
-		$sql2 = "DELETE FROM " . NODES_TABLE . "
-				 WHERE parent_id = " . $node_id;
-		if(SITE_DELETE_LEGAL) {
-			$result1 = $db->sql_query($sql1);
-			$result2 = $db->sql_query($sql2);
-			if(!$result1 || !$result2) {
-				trigger_error(__('Sorry, this isn\'t going to work.'), E_USER_ERROR);
-				define('ERROR', true);
-			}
-		}
 		else {
+			define('SITE_DELETE_LEGAL', true);
+		}
+		$node = CMS_Node::getnew();
+		$node->node_id = $node_id;
+		$node->read();		
+		if($node->type == 'site' && !SITE_DELETE_LEGAL) {
 			trigger_error(__("Can't delete the node. Are you trying to delete a site?"), E_USER_ERROR);
 			define('ERROR', true);
 		}
-		
+		else {
+			delete_node($node_id);
+		}
+
 		if(!defined('ERROR')) {
 			header('Location: ' . utils::base() . 'index.php');		
 		}
@@ -69,11 +62,43 @@ switch($mode) {
 		include('./header.php');
 		?>
 		<form action="?mode=confirmed" method="post">
-			<div style="color: red;"><?php echo __("Are you sure you want to delete this node? This will not remove any data."); ?></div>
+			<div style="color: red;"><?php echo __("Are you sure you want to delete this node? This will remove any data and children."); ?></div>
 			<input type="hidden" name="node_id" value="<?php echo $node_id; ?>" />
 			<input type="submit" name="submit" value="<?php echo __("Submit"); ?>" />
 		</form>
 		<?php include('./footer.php');
 	break;
-}		
+}
+
+function delete_node($node_id) {
+	global $db;
+	$sql = 'DELETE FROM ' . NODES_TABLE . "
+			WHERE node_id = $node_id";
+	if(!$db->sql_query($sql)) {
+		return false;
+	}
+	$sql = 'DELETE FROM ' . NODE_REVISIONS_TABLE . "
+			WHERE node_id = $node_id";
+	if(!$db->sql_query($sql)) {
+		return false;
+	}
+	$sql = 'SELECT node_id FROM ' . NODES_TABLE . "
+			WHERE parent_id = $node_id";
+	if(!$result = $db->sql_query($sql)) {
+		return false;
+	}
+	$affected_rows = $db->sql_affectedrows();
+	if($affected_rows < 1) {
+		$rowset = $db->sql_fetchrowset($result);
+		foreach($rowset as $row) {
+			delete_node($row['node_id']);
+		}
+	}
+	elseif($affected_rows == 1) {
+		$row = $db->sql_fetchrow($result);
+		delete_node($row['node_id']);
+	}
+		
+	return true;
+}
 ?>
