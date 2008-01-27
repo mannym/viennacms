b525371d67b82cd3eca700871dd47481a14457f7
bas <bas@83a8008e-574f-11de-8832-3f4c773c73ae>
2008-01-27 08:46:37 +0000

    {viennafile} tag handler, needs to be placed in the wysiwyg editor still :)

--- includes/utils.php
+++ includes/utils.php
@@ -163,7 +163,9 @@ class utils {
 	static function handle_text($text) {
 		$text = urldecode($text);
 		$page = page::getnew();
+		$files = self::load_extension('files');
 		$text = preg_replace_callback('@href="\{node\:([0-9]+)\}"@', array(&$page, 'get_correct_link'), $text);
+		$text = preg_replace_callback('@{viennafile\:([0-9]+)\}@', array(&$files, 'get_widget'), $text);
 		return $text;
 	}
 	
