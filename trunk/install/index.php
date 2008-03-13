75be083f0c41cfd0563e7a148540934975419849
bas <bas@83a8008e-574f-11de-8832-3f4c773c73ae>
2008-03-13 11:25:22 +0000

    Unit tests! :D (and seemingly a bug fix :|)

--- trunk/install/index.php
+++ trunk/install/index.php
@@ -2,6 +2,7 @@
 define('IN_VIENNACMS', true);
 define('IN_INSTALL', true);
 include('../start.php');
+include(ROOT_PATH . 'includes/functions_install.php');
 
 if (filesize(ROOT_PATH . 'config.php') > 10) {
 	header('Location: ../');
@@ -115,49 +116,10 @@ switch ($step) {
 		$db->sql_connect($dbhost, $dbuser, $dbpasswd, $dbname);
 		$db->prefix = $table_prefix;
 		utils::config_file_write($dbhost, $dbuser, $dbpasswd, $dbname, $table_prefix);	
-		include(ROOT_PATH . 'includes/sql_parse.php');
-		$dbms_schema = 'install.sql';
-		$sql_query = file_get_contents($dbms_schema);
-		$sql_query = preg_replace('#viennacms_#is', $table_prefix, $sql_query);		
-		$sql_query = remove_comments($sql_query);
-		$sql_query = remove_remarks($sql_query);
-		$sql = split_sql_file($sql_query,';');
-		unset($sql_query);
-		include(ROOT_PATH . "includes/constants_core.php");
-		$csql = "SELECT * FROM " . NODES_TABLE;
-		$installed = false;
-		// no dbal here, we need to do raw checks
-		if (@mysql_query($csql)){
-			$installed = true;
-		} else {
-			$error = mysql_error();
-			if(!preg_match("#" . preg_quote("#1146 - Table '(.*?)' doesn't exist","#") . "#",$error)){
-				$installed = false;
-			}else{
-				$installed = true;
-			}
+		$result = install_database($db->prefix);
+		if ($result) { // got an error
+			install_die($result);
 		}
-		if($installed){
-			install_die(__('viennaCMS is already installed there. Please try another database or table prefix.'));
-		}
-		
-		$language = '';
-		
-		if (isset($_COOKIE['language'])) {
-			if ($_COOKIE['language'] != 'english') {
-				$language = addslashes($_COOKIE['language']);
-			}
-		}
-		
-		$sql[] = "UPDATE ".USER_TABLE." SET username = '$name2', password = '$ww2', lang = '$language'";		
-		include(ROOT_PATH . 'includes/version.php');
-		$sql[] = "INSERT INTO " . NODE_OPTIONS_TABLE . " SET node_id = 0, option_name = 'database_version', option_value = '$database_version'";
-
-		foreach ($sql as $query) {
-			if(!$db->sql_query($query)){
-				install_die("Could not insert SQL. Error: " . mysql_error());	
-			}	
-		}	
 	break;	
 	case 4:
 		header('Location: ../index.php');
