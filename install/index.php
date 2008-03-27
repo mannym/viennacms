34451e54a13af07f904694f810d298dccc639316
salomon <salomon@83a8008e-574f-11de-8832-3f4c773c73ae>
2008-03-27 17:02:03 +0000

    Some fixes

--- install/index.php
+++ install/index.php
@@ -115,11 +115,11 @@ switch ($step) {
 		$db = database::getnew();
 		$db->sql_connect($dbhost, $dbuser, $dbpasswd, $dbname);
 		$db->prefix = $table_prefix;
-		utils::config_file_write($dbhost, $dbuser, $dbpasswd, $dbname, $table_prefix);	
 		$result = install_database($db->prefix, $name2, $ww2);
 		if ($result) { // got an error
 			install_die($result);
 		}
+		utils::config_file_write($dbhost, $dbuser, $dbpasswd, $dbname, $table_prefix);	
 	break;	
 	case 4:
 		header('Location: ../index.php');
