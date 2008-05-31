41fbbbbc23b686d3a579011762d1a0e2f3be7ba5
bas <bas@83a8008e-574f-11de-8832-3f4c773c73ae>
2008-05-31 06:36:58 +0000

    More form-ification

--- adm/admin_node_options.php
+++ adm/admin_node_options.php
@@ -41,6 +41,44 @@ if(in_array($node->type, array('site', 'page', 'newsfolder'))) {
 
 $page_title = __('viennaCMS ACP - Node options');
 
+class node_options_form extends form {
+	function submit($fields) {
+		global $node, $easy, $db, $options;
+		
+		foreach ($fields as $key => $value) {
+			if($value == '--' . __('Select') . '--')
+			{
+				$value = '';
+			}
+			if (isset($options[$key])) {
+				$sql = "SELECT * FROM " . NODE_OPTIONS_TABLE . "
+				WHERE node_id = " . $node->node_id . "
+				AND option_name = '" . $key . "'";
+				$result = $db->sql_query($sql);
+				$row = $db->sql_fetchrow($result);
+				$add = ($row === false) ? true : false;
+				$node->write_option($key, $value, $add);
+			}
+		}
+		
+		utils::get_types();
+		
+		if (!$easy) {
+			header('Location: ' . utils::base() . 'admin_node.php?node=' . $node->node_id);
+		} else {
+			if (utils::$types[$node->type]['type'] == NODE_MODULES) {
+				header('Location: ' . utils::base() . 'admin_node_modules.php?easy=true&node=' . $node->node_id);
+			} else if (utils::$types[$node->type]['type'] == NODE_CONTENT) {
+				header('Location: ' . utils::base() . 'admin_node_content.php?easy=true&node=' . $node->node_id);
+			} else {
+				header('Location: ' . utils::base() . 'admin_node.php?node=' . $node->node_id);
+			}
+		}
+	}
+}
+
+ob_start();
+
 switch($mode) {
 	case 'save':
 		error_reporting(E_ALL);
@@ -79,13 +117,33 @@ switch($mode) {
 	default:
 		include('./header.php');
 		if (!$easy) {
-		?>
-		<h1><?php echo sprintf(__('Edit options for %s'), $node->title); ?></h1>
-		<?php
+			$title = sprintf(__('Edit options for %s'), $node->title);
 		} else {
-			echo '<h1>' . sprintf(__('Content wizard, step %d of %d'), 3, 4) . '</h1>';	
+			$title = sprintf(__('Content wizard, step %d of %d'), 3, 4);	
 		}
-		$form = utils::load_extension('form');
+		$form = new node_options_form;
+		$form->elements = array(
+			$title => $options
+		);
+		$form->elements[$title]['node'] = array(
+			'type'			=> 'hidden',
+			'name'			=> 'node',
+			'value'			=> $node->node_id,
+			'raw'			=> true
+		);
+		if ($easy) {
+			$form->elements[$title]['easy'] = array(
+				'type'			=> 'hidden',
+				'name'			=> 'easy',
+				'value'			=> 'true',
+				'raw'			=> true
+			);
+		}
+		$form->form_id = 'node_options_form';
+		$form->action = '?mode=form';
+		$api = new formapi;
+		echo $api->get_form($form);
+		/*$form = utils::load_extension('form');
 		$form->action = '?mode=save';
 		$form->submit = __('Save');
 		$form->setformfields($options);
@@ -103,7 +161,7 @@ switch($mode) {
 		}
 		$form->title = __('Options for this node');
 		$form->generateform();
-		echo $form->content;
+		echo $form->content;*/
 		include('./footer.php');
 	break;
 }
