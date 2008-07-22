d2f82c3602bbb09f4babd53ed2410d40633093a6
bas <bas@83a8008e-574f-11de-8832-3f4c773c73ae>
2008-07-22 14:53:12 +0000

    refs #23

--- build/tests/002_nodesystem.php
+++ build/tests/002_nodesystem.php
@@ -9,6 +9,18 @@ class TestCase_nodes extends PHPUnit_Framework_TestCase {
     protected function tearDown() {
 
     }
+    
+    public function testCreateNode() {
+    	$node = new CMS_Node();
+    	$node->parent_id = 1;
+    	$node->title = 'First node';
+    	$node->description = '';
+    	$node->title_clean = 'first-node';
+    	$node->type = 'page';
+    	$node->write();
+    	
+    	$this->assertEquals(2, $node->node_id, 'Node ID is not 2');
+    }
 
     public function startSystem() {
     	global $cache, $db, $template; // for not die-ing with a fatal error
