7e6bb89fb364b49a3392bf59f28982b44a090dd3
salomon <salomon@83a8008e-574f-11de-8832-3f4c773c73ae>
2008-08-25 11:37:21 +0000

    I hate svn eol style :P had to revert index.php

--- viennacms2/trunk/index.php
+++ viennacms2/trunk/index.php
@@ -3,6 +3,7 @@
 * viennaCMS2 loader file
 * 
 * @package viennaCMS2
+* @version $Id$
 * @copyright (c) 2008 viennaCMS group
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 */
