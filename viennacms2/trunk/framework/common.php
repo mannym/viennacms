357114726e1074cd4fa3fe2e03deed823b319cfb
bas <bas@83a8008e-574f-11de-8832-3f4c773c73ae>
2008-11-04 18:42:22 +0000

    Fixes things that shouldn\'t happen ;)

--- viennacms2/trunk/framework/common.php
+++ viennacms2/trunk/framework/common.php
@@ -33,6 +33,68 @@ if (version_compare(phpversion(), '6.0.0-dev', '<') && get_magic_quotes_gpc()) {
 	define('STRIP', false);
 }
 
+if (STRIP) {
+	if( is_array($_GET) )
+	{
+		while( list($k, $v) = each($_GET) )
+		{
+			if( is_array($_GET[$k]) )
+			{
+				while( list($k2, $v2) = each($_GET[$k]) )
+				{
+					$_GET[$k][$k2] = stripslashes($v2);
+				}
+				@reset($_GET[$k]);
+			}
+			else
+			{
+				$_GET[$k] = stripslashes($v);
+			}
+		}
+		@reset($_GET);
+	}
+	
+	if( is_array($_POST) )
+	{
+		while( list($k, $v) = each($_POST) )
+		{
+			if( is_array($_POST[$k]) )
+			{
+				while( list($k2, $v2) = each($_POST[$k]) )
+				{
+					$_POST[$k][$k2] = stripslashes($v2);
+				}
+				@reset($_POST[$k]);
+			}
+			else
+			{
+				$_POST[$k] = stripslashes($v);
+			}
+		}
+		@reset($_POST);
+	}
+	
+	if( is_array($_COOKIE) )
+	{
+		while( list($k, $v) = each($_COOKIE) )
+		{
+			if( is_array($_COOKIE[$k]) )
+			{
+				while( list($k2, $v2) = each($_COOKIE[$k]) )
+				{
+					$_COOKIE[$k][$k2] = stripslashes($v2);
+				}
+				@reset($_COOKIE[$k]);
+			}
+			else
+			{
+				$_COOKIE[$k] = stripslashes($v);
+			}
+		}
+		@reset($_COOKIE);
+	}
+}
+
 cms::$vars = new GlobalStore();
 //$global = cms::$vars;
 
