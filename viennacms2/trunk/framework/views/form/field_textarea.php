6702ac1249bbe7701ffc2236af8e05323b427dd5
bas <bas@83a8008e-574f-11de-8832-3f4c773c73ae>
2009-04-15 14:11:33 +0000

    Contact form.

--- viennacms2/trunk/framework/views/form/field_textarea.php
+++ viennacms2/trunk/framework/views/form/field_textarea.php
@@ -1,1 +1,1 @@
-<textarea id="<?php echo $this['name'] ?>" name="<?php echo $this['name'] ?>" class="textarea <?php echo $this['class'] ?>"><?php echo $this['value'] ?></textarea>
+<textarea id="<?php echo $this['name'] ?>" name="<?php echo $this['name'] ?>" class="textarea <?php echo $this['class'] ?>" <?php echo $this['attributes'] ?>><?php echo $this['value'] ?></textarea>
